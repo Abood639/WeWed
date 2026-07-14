14d0b5b0a14587c3db8df9aa3491a569e6cf1078
averybrandhyve-del <avery.brandhyve@gmail.com>
2026-07-14 15:56:38 -0400

    Optimize SEO, clean URLs, update form selector, and add robots.txt/sitemap.xml

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import http.server
 import socketserver
+import os
 
 class NoCacheHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
     def end_headers(self):
@@ -7,6 +8,30 @@ class NoCacheHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
         self.send_header('Pragma', 'no-cache')
         self.send_header('Expires', '0')
         super().end_headers()
+
+    def do_GET(self):
+        # Redirect index.html or paths ending in .html to clean URLs
+        if self.path.endswith('.html'):
+            clean_path = self.path[:-5]
+            if not clean_path or clean_path == '/index':
+                clean_path = '/'
+            self.send_response(301)
+            self.send_header('Location', clean_path)
+            self.end_headers()
+            return
+        elif self.path.endswith('/index'):
+            self.send_response(301)
+            self.send_header('Location', '/')
+            self.end_headers()
+            return
+        super().do_GET()
+
+    def translate_path(self, path):
+        translated = super().translate_path(path)
+        if not translated.endswith('.html') and not os.path.isdir(translated):
+            if os.path.exists(translated + '.html'):
+                return translated + '.html'
+        return translated
 
 class ThreadingHTTPServer(socketserver.ThreadingTCPServer):
     allow_reuse_address = True
